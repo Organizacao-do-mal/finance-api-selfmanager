7d135c1d3ecaf2843725793c9985fc0ad3ceae79
elias.oliveira <elias.oliveira@adviseu.com.br>
2023-11-22 15:50:34 -0300

    initial commit

---
+++ src/main/java/com/selfmanager/financeapiselfmanager/FinanceApiSelfmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.selfmanager.financeapiselfmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceApiSelfmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceApiSelfmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/selfmanager/financeapiselfmanager/FinanceApiSelfmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selfmanager.financeapiselfmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceApiSelfmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
